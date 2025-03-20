d1955f3c04267e4a23f8cc25686d205bb6b1f0d7
loopedupl <admin@loop.edu.pl>
2025-03-20 10:23:32 +0100

    Add dockerfile

--- frontend/src/global-config.ts
+++ frontend/src/global-config.ts
@@ -6,6 +6,6 @@ export const CONFIG = {
   appName: packageJson.name,
   appVersion: packageJson.version,
   assetsDir: process.env.NEXT_PUBLIC_ASSETS_DIR ?? "",
-  env: process.env.ENV ?? "LOCAL",
-  isLocal: (process.env.ENV ?? "LOCAL") === "LOCAL",
+  env: process.env.NEXT_PUBLIC_ENV ?? "LOCAL",
+  isLocal: (process.env.NEXT_PUBLIC_ENV ?? "LOCAL") === "LOCAL",
 };
