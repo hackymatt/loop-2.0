95133a9efa4ad04f3d4e4ae1c4c805ae7ac7df3e
loopedupl <admin@loop.edu.pl>
2025-05-04 08:35:13 +0200

    Add model

--- backend/course/enrollment/models.py
+++ backend/course/enrollment/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from core.base_model import BaseModel
 from user.type.student_user.models import Student
 from ..models import Course
+from ..chapter.models import Chapter
 from global_config import CONFIG
 
 
@@ -29,3 +30,28 @@ class CourseEnrollment(BaseModel):
 
     def __str__(self):
         return f"{self.student.user.email} started {self.course.slug} on {self.created_at}"  # pragma: no cover
+
+
+class CourseChapterEnrollment(BaseModel):
+    student = models.ForeignKey(
+        Student,
+        on_delete=models.SET(get_dummy_student),
+        related_name="chapter_enrollments",
+    )
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, related_name="chapter_enrollments"
+    )
+    chapter = models.ForeignKey(
+        Chapter, on_delete=models.CASCADE, related_name="chapter_enrollments"
+    )
+
+    class Meta:
+        db_table = "course_chapter_enrollment"
+        unique_together = (
+            "student",
+            "course",
+            "chapter",
+        )  # Ensures that a student can only enroll in a course chapter once
+
+    def __str__(self):
+        return f"{self.student.user.email} started {self.chapter.slug} of {self.course.slug} on {self.created_at}"  # pragma: no cover
